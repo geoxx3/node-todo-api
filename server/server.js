e0b623b572b031dfd28280cab61684c4e7953e79
geoxx3 <geoxx3@gmail.com>
2017-11-27 07:08:40 +0000

    Add DELETE /todos/:id route

--- server/server.js
+++ server/server.js
@@ -6,7 +6,7 @@ var {mongoose} = require('./db/mongoose');
 var {Todo} = require('./models/todo');
 var {User} = require('./models/user');
 
-var app = express
+var app = express();
 const port = process.env.PORT || 3000;
 
 app.use(bodyParser.json());
@@ -44,6 +44,24 @@ app.get('/todos/:id', (req, res) => {
     }
 
     res.send({todo});
+  }).catch((e) => {
+    res.status(400).send();
+  });
+});
+
+app.delete('/todos/:id', (req, res) => {
+  var id = req.params.id;
+
+  if (!ObjectID.isValid(id)) {
+    return res.status(404).send();
+  }
+
+  Todo.findByIdAndRemove(id).then((todo) => {
+    if (!todo) {
+      return res.status(404).send();
+    }
+
+    res.send(todo);
   }).catch((e) => {
     res.status(400).send();
   });
