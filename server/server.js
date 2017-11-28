65d036db294ea3c8a8e6bef72013a5af85514554
geoxx3 <geoxx3@gmail.com>
2017-11-28 09:44:57 +0000

    Add POST /users route and token method

--- server/server.js
+++ server/server.js
@@ -1,4 +1,4 @@
-require('./config.config');
+require('./config/config');
 
 const _ = require('lodash');
 const express = require('express');
@@ -93,6 +93,20 @@ app.patch('/todos/:id', (req, res) => {
     res.send({todo});
   }).catch((e) => {
     res.status(400).send();
+  })
+});
+
+// POST /users
+app.post('/users', (req, res) => {
+  var body = _.pick(req.body, ['email', 'password']);
+  var user = new User(body);
+
+  user.save().then(() => {
+    return user.generateAuthToken();
+  }).then((token) => {
+    res.header('x-auth', token).send(user);
+  }).catch((e) => {
+    res.status(400).send(e);
   })
 });
 
